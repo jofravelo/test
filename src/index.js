f1a13b9abfff796722d0fca46c7e9751cb5fbcb1
JF Ravelo <jf.ravelo@gmail.com>
2019-04-19 17:20:10 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hi!</h1>
+<div>
+  We use Pl to this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
